cb3c9e85393bccf707bd8a64c2803303a6816d80
+ <+>
2022-10-18 11:20:19 +0100

    commit tree ec23e4c1300e966272e430a7ea15dfe9d1c5ab4a

--- index.js
+++ index.js
@@ -1,4 +1,100 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+    const employeeRecord = {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+
+    return employeeRecord;
+}
+
+function createEmployeeRecords(allArrays) {
+    const allEmployees = []
+    for (let arrOfArrays of allArrays) {
+        const allEmployeeRecords = createEmployeeRecord(arrOfArrays)
+        allEmployees.push(allEmployeeRecords)
+    }
+
+    return allEmployees;
+}
+
+function createTimeInEvent(dateInStamp) {
+    const dateInArray = dateInStamp.split(" ");
+    const timeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateInArray[1]),
+        date: dateInArray[0]
+    }
+    
+    this.timeInEvents.push(timeIn);
+
+    return this;
+}
+
+function createTimeOutEvent(dateOutStamp) {
+    const dateOutArray = dateOutStamp.split(" ");
+    const timeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateOutArray[1]),
+        date: dateOutArray[0]
+    }
+    
+    this.timeOutEvents.push(timeOut);
+
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    for (let i = 0; i < (this.timeInEvents).length; i++) {
+        let dateToCheck = this.timeInEvents[i].date
+        if (dateStamp === dateToCheck) {
+            const hourIn = this.timeInEvents[i].hour
+            const hourOut = this.timeOutEvents[i].hour
+
+            const numberOfHoursWorked = (hourOut - hourIn)/100;
+            return numberOfHoursWorked;
+        }
+    }
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    const payRate = this.payPerHour
+    const hoursWorked = hoursWorkedOnDate.call(this,dateStamp)
+
+    const payOwed = payRate * hoursWorked
+    return payOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    for (let src of srcArray) {
+        if (src.firstName === firstName) {
+            return src;
+        }
+    }
+}
+
+function calculatePayroll(allEmployees) {
+    const fullPayroll = [];
+    for (let i=0; i< allEmployees.length; i++) {
+        const totalWageForEmployee = allWagesFor.call(allEmployees[i])
+        fullPayroll.push(totalWageForEmployee)
+    }
+    
+
+    let fullPay = 0;
+    for (const pay of fullPayroll) {
+        fullPay += pay
+    }
+
+    return fullPay;
+
+}
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
